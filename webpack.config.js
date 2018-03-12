6eaf21f17fd2b97c66791c2ccc98a64431691230
Andres Aguilar <andresyoshimar@gmail.com>
2018-03-12 11:55:30 -0600

    Agregando archivos de configuracion

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: './assets/index.template.html',
+  filename: 'index.html'
+});
+
+module.exports = {
+  entry: './assets/scripts/entry.js',
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname, '..'),
+    filename: 'dist/scripts/bundle.js'
+  },
+  mode: 'development',
+  plugins: [htmlWebpackPlugin],
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    }]
+  }
+};
