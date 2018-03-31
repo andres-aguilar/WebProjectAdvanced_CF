223270e61d2f4c575c7d1f93f699e20c8afbf315
Andres Aguilar <andresyoshimar@gmail.com>
2018-03-31 16:34:51 -0600

    Agregando marcador al mapa

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -1,11 +1,31 @@
 function initMap() {
+  const coords = {
+    lat: 19.074996,
+    lng: -98.206573
+  };
+
+  // Inicializado el mapa con las opciones basicas
   let map = new google.maps.Map(document.querySelector("#mapa"), {
+    center: coords,
     zoom: 16,
-    center: {
-      lat: 19.074996,
-      lng: -98.206573
-    }
   });
+
+  // Ventana de informacion
+  /*let infowindow = new google.maps.InfoWindow({
+    content: "CAPU - Como llegar a la terminal de puebla";
+  });*/
+
+  // Creando un marcador
+  let marker = new google.maps.Marker({
+    position: coords,
+    map,
+    title: 'Como llegar'
+  });
+  /*
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });*/
+
 }
 
 initMap();
