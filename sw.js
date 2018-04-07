5efac844def32406f39c8ad42c9f57da54aea5b0
Andres Aguilar <andresyoshimar@gmail.com>
2018-04-06 20:43:44 -0500

    Agregando funcion para guardar en cache todos los archivos del sitio y que funcione aun sin conexion

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'YOSH_CACHE_PLACES_v1';
 self.addEventListener('install', function() {
   // Save files
   caches.open(CACHE_NAME).then(function(cache) {
-    cache.addAll(['./index.html']);
+    cache.addAll(['./index.html', '/dist/scripts/bundle.js']);
   });
 } );
 
@@ -19,4 +19,30 @@ self.addEventListener('activate', function(e) {
       return Promise.all(promises);
     })
   )
+});
+
+function searchInCacheOrMakeRequest(request) {
+  const cachePromise = caches.open(CACHE_NAME);
+  const matchPromise = cachePromise.then(function(cache) {return cache.match(request);});
+
+  return Promise.all([cachePromise, matchPromise]).then(function([cache, cacheResponse]) {
+    const fetchPromise = fetch(request).then(function(fetchResponse) {
+      // Update local cache with new resources
+      cache.put(request, fetchResponse.clone()); // Save resource in cache
+      return fetchResponse;
+    })
+    return cacheResponse || fetchPromise;
+  });
+}
+
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return searchInCacheOrMakeRequest(e.request);
+    }).catch(function(err) {
+      if(e.request.mode == 'navigate')
+        return caches.match(e.request);
+    })
+  );
 });
